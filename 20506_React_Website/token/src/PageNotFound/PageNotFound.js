5ec48737a37b57292204dd68a1087c5f9d84961a
Pavan A <pavan479ec@gmail.com>
2021-11-29 01:16:50 +0530

    React 404 Page updated.

--- 20506_React_Website/token/src/PageNotFound/PageNotFound.js
+++ 20506_React_Website/token/src/PageNotFound/PageNotFound.js
@@ -1,9 +1,23 @@
 import React from "react";
+import notFount from "../Assets/404.svg";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
+    <div
+      className="p-5"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        style={{ width: "auto", height: "100%" }}
+        src={notFount}
+        alt="Page Not Found"
+      />
     </div>
   );
 };
